df1d08a0acdc6348523f86f3d9a250e15506a0c1
rzy11034 <rzy11034@163.com>
2020-02-26 11:59:54 +0800

    2020-02-26 12:00

--- Delphi/Source/Case04_多维数组与矩阵/LQA.Case04_04_边界为1的最大子方阵.pas
+++ Delphi/Source/Case04_多维数组与矩阵/LQA.Case04_04_边界为1的最大子方阵.pas
@@ -22,11 +22,10 @@ procedure Main;
 implementation
 
 // 动态规划
-function MaxSquareADV(const matrix: TArr2D_int): integer;
-
+function MaxSquareADV(const matrix: TArr2D_int; out aux: TArr3D_int): integer;
 // 生成一个辅助表 ret[][][]
 // ret[x，x][0] 为下；ret[x，x][1] 为右；
-  function GenerateAux(const matrix: TArr2D_int): TArr3D_int;
+  function __generateAux(const matrix: TArr2D_int): TArr3D_int;
   var
     rowCount, colCount, i, j: integer;
     ret: TArr3D_int;
@@ -55,23 +54,58 @@ function MaxSquareADV(const matrix: TArr2D_int): integer;
     Result := ret;
   end;
 
-  function Check(r, l, n: integer): Boolean;
+  function __check(r, l, n: integer; const aux: TArr3D_int): boolean;
   begin
+    Result := False;
 
+    // 左上角那个点往下数的1的数目要 >= n
+    // 左上角那个点往右数的1的数目要 >= n
+    // 右上角那个点往下数的1的数目要 >= n
+    // 左下角那个点往右数的1的数目要 >= n
+    if (aux[r, l][0] >= n) and (aux[r, l][1] >= n) and (aux[r, l + n - 1][0] >= n)
+      and (aux[r + n - 1, l][1] >= n) then
+    begin
+      Result := True;
+      Exit;
+    end;
   end;
 
 var
-  rowCount, colCount, i, j: integer;
-  aux: TArr3D_int;
+  len, n, i, j: integer;
 begin
-  aux := GenerateAux(matrix);
-  TArrayUtils_int.Print3D(aux);
-  Result := 0;
+  len := Length(matrix);
+  n := len;
+  aux := __generateAux(matrix);
+
+  while n > 0 do
+  begin
+    for i := 0 to len - 1 do
+    begin
+      if i + n > len then
+        Break;
+
+      for j := 0 to len - 1 do
+      begin
+        if j + n > len then
+          Break;
+
+        if __check(i, j, n, aux) then
+        begin
+          Result := n;
+          Exit;
+        end;
+      end;
+    end;
+
+    Dec(n);
+  end;
+
+  Result := n;
 end;
 
 // 枚举法（暴力求解）————O(n³)
 function MaxSquare(const matrix: TArr2D_int): integer;
-label L3;
+label L2, L3;
 var
   len, n, r, c, i, j: integer;
 begin
@@ -83,7 +117,6 @@ begin
   begin
     for i := 0 to len - 1 do
     begin
-
       for j := 0 to len - 1 do
       begin
         r := i;
@@ -92,6 +125,9 @@ begin
         // 上
         while c < j + n do
         begin
+          if c >= len then
+            goto L2;
+
           if matrix[r][c] = 0 then
             goto L3;
 
@@ -102,6 +138,9 @@ begin
         Dec(c);
         while r < i + n do
         begin
+          if r >= len then
+            goto L2;
+
           if matrix[r][c] = 0 then
             goto L3;
 
@@ -129,41 +168,52 @@ begin
         end;
 
         Exit(n);
+
+      L3:
+        Continue;
       end;
 
-    L3:
+    L2:
       Continue;
     end;
 
     Dec(n);
   end;
+
+  Result := n;
 end;
 
 procedure Main;
 var
   matrix: TArr2D_int;
   aux: TArr3D_int;
+  n: integer;
 begin
   matrix := [
-  //[0, 1, 1, 1, 1],
-  //[0, 1, 0, 0, 1],
-  //[0, 1, 0, 0, 1],
-  //[0, 1, 1, 1, 1],
-  //[0, 1, 0, 1, 1]];
+    //[0, 1, 1, 1, 1],
+    //[0, 1, 0, 0, 1],
+    //[0, 1, 0, 0, 1],
+    //[0, 1, 1, 1, 1],
+    //[0, 1, 0, 1, 1]];
 
-  //[1, 1, 1, 1],
-  //[1, 0, 0, 1],
-  //[1, 0, 0, 1],
-  //[1, 1, 1, 1]];
+    //[1, 1, 1, 1],
+    //[1, 0, 0, 1],
+    //[1, 0, 0, 1],
+    //[1, 1, 1, 1]];
 
-    [0, 1],
-    [1, 1]];
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]];
 
   TArrayUtils_int.Print2D(matrix);
   DrawLineBlockEnd;
-  WriteLn(MaxSquare(matrix));
-  //MaxSquareADV(matrix);
 
+  WriteLn(MaxSquare(matrix));
+  DrawLineBlockEnd;
+
+  n := MaxSquareADV(matrix, aux);
+  TArrayUtils_int.Print3D(aux);
+  WriteLn(n);
 end;
 
 end.
