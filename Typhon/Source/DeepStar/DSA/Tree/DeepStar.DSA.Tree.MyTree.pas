e10fe1f4ad74a6b660f8d46bb071d35b66a5a1ef
rzy11034 <rzy11034@163.com>
2020-04-20 00:10:23 +0800

    2020-04-20 0:00

---
+++ Typhon/Source/DeepStar/DSA/Tree/DeepStar.DSA.Tree.MyTree.pas
@@ -0,0 +1,71 @@
+﻿unit DeepStar.DSA.Tree.MyTree;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  {%H-}Rtti,
+  DeepStar.DSA.Linear.DoubleLinkedList,
+  DeepStar.DSA.Interfaces,
+  DeepStar.Utils.UString;
+
+type
+  generic TMyTree<T> = class(TObject)
+
+    TList_T = specialize TDoubleLinkedList<T>;
+    TImpl_T = specialize TImpl<T>;
+
+  public type
+    TTreeNode = class(TObject)
+    private type
+      TList_TreeNode = specialize TDoubleLinkedList<TTreeNode>;
+
+    public
+      Key: T;
+      Parent: TTreeNode;
+      Children: TList_TreeNode;
+
+      constructor Create(newKey: T; newParent: TTreeNode = nil);
+      destructor Destroy; override;
+    end;
+
+  private
+    _
+
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+  end;
+
+implementation
+
+{ TMyTree }
+
+constructor TMyTree.Create;
+begin
+
+end;
+
+destructor TMyTree.Destroy;
+begin
+  inherited Destroy;
+end;
+
+{ TMyTree.TTreeNode }
+
+constructor TMyTree.TTreeNode.Create(newKey: T; newParent: TTreeNode);
+begin
+  Key := newKey;
+  Parent := newParent;
+end;
+
+destructor TMyTree.TTreeNode.Destroy;
+begin
+  inherited Destroy;
+end;
+
+end.
