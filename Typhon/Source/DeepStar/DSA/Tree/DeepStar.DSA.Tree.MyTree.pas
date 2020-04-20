7ed5317a977f503e583d4fa80bfe484743e085f0
rzy11034 <rzy11034@163.com>
2020-04-21 00:34:21 +0800

    2020-04-21 0:34

--- Typhon/Source/DeepStar/DSA/Tree/DeepStar.DSA.Tree.MyTree.pas
+++ Typhon/Source/DeepStar/DSA/Tree/DeepStar.DSA.Tree.MyTree.pas
@@ -7,7 +7,7 @@ interface
 uses
   Classes,
   SysUtils,
-  {%H-}Rtti,
+  Math,
   DeepStar.DSA.Linear.ArrayList,
   DeepStar.DSA.Interfaces,
   DeepStar.Utils.UString;
@@ -15,13 +15,12 @@ uses
 type
   generic TMyTree<T> = class(TObject)
   private type
-    TList_T = specialize TArrayList<T>;
+    TTreeNode = class;
     TImpl_T = specialize TImpl<T>;
+    TList_TreeNode = specialize TArrayList<TTreeNode>;
 
   public type
     TTreeNode = class(TObject)
-    private type
-      TList_TreeNode = specialize TArrayList<TTreeNode>;
     public
       Key: T;
       Parent: TTreeNode;
@@ -37,33 +36,36 @@ type
     _root: TTreeNode;
 
   public
-    constructor Create;
+    constructor Create(root: TTreeNode);
     destructor Destroy; override;
 
     function Count: cardinal;
-    function GetChildren: TTreeNode.TList_TreeNode;
     function GetFirstChild(node: TTreeNode): TTreeNode;
     function GetHeight(node: TTreeNode): integer; overload;
     function GetHeight: integer; overload;
     function GetNextSibling(node: TTreeNode): TTreeNode;
+    function GetParent(node: TTreeNode): TTreeNode;
     function GetRoot: TTreeNode;
-    function LevelOrder(node: TTreeNode): TTreeNode.TList_TreeNode; overload;
-    function LevelOrder: TTreeNode.TList_TreeNode; overload;
-    function PostOrder: TTreeNode.TList_TreeNode;
-    function PreOrder: TTreeNode.TList_TreeNode;
-    procedure InsertChild(parent, child: TTreeNode);
+    function LevelOrder(node: TTreeNode): TList_TreeNode; overload;
+    function LevelOrder: TList_TreeNode; overload;
+    function PosCtOrder: TList_TreeNode;
+    function PreOrder: TList_TreeNode;
     procedure DeleteChild(parent: TTreeNode; i: integer);
+    procedure InsertChild(parent, child: TTreeNode);
+
+    property Comparer: TImpl_T.ICmp read _cmp write _cmp;
   end;
 
 implementation
 
 { TMyTree }
 
-constructor TMyTree.Create;
+constructor TMyTree.Create(root: TTreeNode);
 begin
-  _root := nil;
   _size := 0;
   _cmp := TImpl_T.TCmp.Default;
+  _root := root;
+  _size += 1;
 end;
 
 function TMyTree.Count: cardinal;
@@ -81,39 +83,54 @@ begin
   inherited Destroy;
 end;
 
-function TMyTree.GetChildren: TTreeNode.TList_TreeNode;
-begin
-
-end;
-
 function TMyTree.GetFirstChild(node: TTreeNode): TTreeNode;
 begin
-
+  Result := node.Children.GetFirst;
 end;
 
 function TMyTree.GetHeight(node: TTreeNode): integer;
+var
+  h, i: integer;
 begin
+  if node.Children = nil then
+    Exit(0);
 
+  h := 0;
+  for i := 0 to node.Children.Count - 1 do
+  begin
+    h := Max(h, GetHeight(node.Children[i]));
+  end;
+
+  Result := h + 1;
 end;
 
 function TMyTree.GetHeight: integer;
 begin
-
+  Result := GetHeight(_root);
 end;
 
 function TMyTree.GetNextSibling(node: TTreeNode): TTreeNode;
+var
+  list: TList_TreeNode;
+  i: integer;
 begin
+  list := node.Parent.Children;
+  i := list.IndexOf(node);
 
+  if i = list.Count - 1 then
+    Exit(nil);
+
+  Result := list.Items[i + 1];
+end;
+
+function TMyTree.GetParent(node: TTreeNode): TTreeNode;
+begin
+  Result := node.Parent;
 end;
 
 function TMyTree.GetRoot: TTreeNode;
 begin
-
-end;
-
-procedure TMyTree.Insert(key: T; parent: TTreeNode);
-begin
-
+  Result := _root;
 end;
 
 procedure TMyTree.InsertChild(parent, child: TTreeNode);
@@ -121,27 +138,22 @@ begin
 
 end;
 
-function TMyTree.LevelOrder(node: TTreeNode): TTreeNode.TList_TreeNode;
+function TMyTree.LevelOrder(node: TTreeNode): TList_TreeNode;
 begin
 
 end;
 
-function TMyTree.LevelOrder: TTreeNode.TList_TreeNode;
+function TMyTree.LevelOrder: TList_TreeNode;
 begin
 
 end;
 
-function TMyTree.PostOrder: TTreeNode.TList_TreeNode;
+function TMyTree.PosCtOrder: TList_TreeNode;
 begin
 
 end;
 
-function TMyTree.PreOrder: TTreeNode.TList_TreeNode;
-begin
-
-end;
-
-procedure TMyTree.Remove(key: T);
+function TMyTree.PreOrder: TList_TreeNode;
 begin
 
 end;
